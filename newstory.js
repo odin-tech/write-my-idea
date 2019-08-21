71b198849377d89803a861340eafbb900fdc47b2
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2019-08-21 14:52:51 +1200

    Updated badword list.

--- newstory.js
+++ newstory.js
@@ -1,7 +1,7 @@
 var helpOpen = false;
 var ideaNum = 1;
 var ideas = ["", "", "", "", ""];
-var banwords = ["poo ", "fuck", "shit", "ass "];
+var banwords = ["poo ", "fuck", "shit", "ass ", "cunt ", "wank", "piss "];
 var title = "";
 var author = "";
 var numbers = ["First", "Second", "Third", "Fourth", "Fifth"];
