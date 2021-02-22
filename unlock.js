0b3b60a4579f6c8f96eb0531157859350568b2ba
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2021-02-23 12:28:55 +1300

    Fixed a security hole (o_o)

--- unlock.js
+++ unlock.js
@@ -1,14 +1,19 @@
-var code = "";
 var alerted = false;
 
-var codelist = [["A55371", "T76723", "F01910", "G85967", "C01839"], ["D84860", "P47214", "L49203", "B52333", "V72535"], ["N91750", "M16788", "Z10483", "X02992", "Q15933"], ["H64260", "K10849", "H35581", "D30622", "E42742"], ["B27957", "U46510", "J31682", "Y03635", "S56103"]];
+var codelist = [
+	["A55371", "T76723", "F01910", "G85967", "C01839"],
+	["D84860", "P47214", "L49203", "B52333", "V72535"],
+	["N91750", "M16788", "Z10483", "X02992", "Q15933"],
+	["H64260", "K10849", "H35581", "D30622", "E42742"],
+	["B27957", "U46510", "J31682", "Y03635", "S56103"]
+];
 
 function requestCodes() {
 	var coordtext = document.getElementById("coordtext");
 	var col = Math.floor((Math.random() * 5));
 	var row = Math.floor((Math.random() * 5));
 	var displaycol = "";
-	code = codelist[row][col];
+	p = codelist[row][col];
 	switch (col) {
 		case 0:
 			displaycol = "A";
@@ -45,14 +50,15 @@ function checkForAuthorisation() {
 }
 
 function init() {
+	var p = "";
 	setTimeout(function () {
 		document.getElementById("codeentry").className = "normal";
 		checkForAuthorisation();
 		setInterval(function () {
 			if (document.getElementById("codeentry").value.length == 6) {
-				if (document.getElementById("codeentry").value.toUpperCase() == code) {
+				if (document.getElementById("codeentry").value.toUpperCase() == p) {
 					document.getElementById("codeentry").className = "correct";
-					localStorage.setItem("auth", code);
+					localStorage.setItem("auth", p);
 					if (!alerted) {
 						alert("Device Authorised!\n\nThis device has been successfully authorised. You won't need to enter a code again (unless you clear your browser data).\n\nTo deauthorise this device, tap About on the main menu and tap Remove Key.\n\nTap OK/Close to continue to the menu.");
 						setTimeout(function () {
